61db7178ca75895ba9e94aa7ae60bd162ff72aee
bismuth00 <bismuth00@gmail.com>
2025-05-17 13:04:33 +0900

    UIの改善: Cosmosクラスのbuildメソッドでshelf_tableの構造を整理し、管理カテゴリーセクションのレイアウトを調整

--- cosmos.py
+++ cosmos.py
@@ -15,14 +15,15 @@ class Cosmos(ft.Container):
 
     def build(self):
         shelf_table = ft.DataTable(
-                    columns = [
-                        ft.DataColumn(ft.Text("")),
-                        ft.DataColumn(ft.Text("管理番号")),
-                        ft.DataColumn(ft.Text("カテゴリ")),
-                        ft.DataColumn(ft.Text("書名")),
-                        ft.DataColumn(ft.Text("場所")),
-                    ],
-                )
+            sort_column_index=1,
+            columns = [
+                ft.DataColumn(ft.Text("")),
+                ft.DataColumn(ft.Text("管理番号")),
+                ft.DataColumn(ft.Text("カテゴリ")),
+                ft.DataColumn(ft.Text("書名")),
+                ft.DataColumn(ft.Text("場所")),
+            ],
+        )
         for book in self.books:
             shelf_table.rows.append(
                 ft.DataRow(
@@ -37,17 +38,21 @@ class Cosmos(ft.Container):
             )
 
         self.content = ft.Column(controls=[
+            ft.Divider(color=ft.Colors.TRANSPARENT),
             ft.ResponsiveRow([
-                ft.Column(col=6, controls=[ft.Text("管理カテゴリー", theme_style=ft.TextThemeStyle.TITLE_LARGE)]),
+                ft.Column(col=7, controls=[ft.Text("管理カテゴリー", theme_style=ft.TextThemeStyle.TITLE_LARGE)]),
                 ft.CupertinoSlidingSegmentedButton(
-                    col=6,
+                    col=5,
                     selected_index=1,
                     controls=[
                         ft.Text("無印"),
+                        ft.Text("ISBN"),
                         ft.Text("ＢＫ"),
                         ft.Text("ＤＶ"),
                         ft.Text("ＨＶ"),
                     ],
                 )
             ]),
-            shelf_table], scroll=ft.ScrollMode.AUTO, expand=True, horizontal_alignment=ft.CrossAxisAlignment.STRETCH)
+            ft.Column(controls=[shelf_table], scroll=ft.ScrollMode.AUTO, expand=True, horizontal_alignment=ft.CrossAxisAlignment.STRETCH)
+        ])
+
